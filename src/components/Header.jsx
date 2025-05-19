f0116182530478c1d507589d377ffc77a36887be
Wilker Quirino <wilkerquirino18@hotmail.com>
2025-05-18 21:44:31 -0400

    Atualizando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,27 @@ import "../style.css";
 const Header = () => {
   return (
     <div className="header-background__image">
-      <div>
-        <header className="container">
-          <div className="button-login">
-            <Link to="/login" className="login">
-              Login
-            </Link>
-          </div>
-          <div className="lobo">
-            <img src={lobo} alt="Lobo com cicatris" />
-          </div>
-          <div>
-            <h1>7 LOBOS M.C.</h1>
-          </div>
-          <div className="top-bar">
-            <Link to="/galeria" className="button-gallery">
-              Galeria
-            </Link>
-            <Link to="/noticias" className="button-news">
-              Notícias
-            </Link>
-          </div>
-        </header>
-      </div>
+      <header className="container">
+        <div className="button-login">
+          <Link to="/login" className="login">
+            Login
+          </Link>
+        </div>
+        <div className="lobo">
+          <img src={lobo} alt="Lobo com cicatris" />
+        </div>
+        <div>
+          <h1>7 LOBOS M.C.</h1>
+        </div>
+        <div className="top-bar">
+          <Link to="/galeria" className="button-gallery">
+            Galeria
+          </Link>
+          <Link to="/noticias" className="button-news">
+            Notícias
+          </Link>
+        </div>
+      </header>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <header className="container-login">
+      <h1 className="text-login">Faça seu login</h1>
+      <section>
+        <div className="login-space">
+          <input id="email" name="email" type="text" placeholder="Email" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+          <input type="submit" value="ENTRAR" />
+          <a href="/recuperar-senha" className="forgot-password">
+            Esqueci minha senha!
+          </a>
+        </div>
+      </section>
+    </header>
+  );
 };
 
 export default Login;
