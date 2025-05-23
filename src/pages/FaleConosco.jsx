a7d3b662d31d40fcb2b6605f3ed83809f6ce2e97
Wilker Quirino <wilkerquirino18@hotmail.com>
2025-05-23 17:34:38 -0400

    Atualizando

--- src/pages/FaleConosco.jsx
+++ src/pages/FaleConosco.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
-const FaleConosco = () => {
-  return <div>FaleConosco</div>;
-};
+function FaleConosco() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container-speak">
+      <div className="voltar-container">
+        <div className="voltar" onClick={() => navigate(-1)}>
+          <FontAwesomeIcon icon={faCircleLeft} />
+        </div>
+        <div className="form-speak">
+          <h2>Fale Conosco</h2>
+          <p>Preencha o formulário e entraremos em contato!</p>
+          <form>
+            <label htmlFor="nome">
+              Nome
+              <span className="mandatory"> (obrigatório)</span>
+              <input type="text" required />
+            </label>
+
+            <label htmlFor="email">
+              E-mail
+              <span className="mandatory"> (obrigatório)</span>
+              <input type="email" required />
+            </label>
+
+            <label htmlFor="assunto">
+              Assunto
+              <span className="mandatory"> (obrigatório)</span>
+              <input type="text" required />
+            </label>
+
+            <label htmlFor="mensagem">
+              <textarea
+                placeholder="Escreva sua mensagem..."
+                required
+              ></textarea>
+            </label>
+            <button type="submit" className="submit-button">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default FaleConosco;
